d6028e3d07626f36faa4984305c13685e309351c
palani-24 <p94509107@gmail.com>
2026-07-30 20:36:58 +0530

    Update Admin password credentials to admin@1234

--- backend/utils/seed.js
+++ backend/utils/seed.js
@@ -51,7 +51,7 @@ const seedDatabase = async () => {
     const adminUser = await User.create({
       name: 'System Admin',
       email: 'admin@ecoreward.com',
-      password: 'EcoAdmin#2026!Pass',
+      password: 'admin@1234',
       role: 'admin',
       profileImage: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?w=150'
     });
